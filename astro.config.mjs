488dfc9addc21d296dd877126e01aa69bf843f35
jordigabarroarmengol <jordigabarroarmengol@gmail.com>
2024-07-29 14:54:10 +0000

    netlify settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 
+// https://astro.build/config
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-
+  output: "server",
+  adapter: netlify()
 });
